daf5a052ad245c6c9959766f62e822b9597706e3
jessiicamaru <Dandrew2407@gmail.com>
2025-09-22 15:38:21 +0700

    load raw html

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.com.webbrowser {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires org.jsoup;
 
     opens org.com.webbrowser to javafx.fxml;
     exports org.com.webbrowser;
